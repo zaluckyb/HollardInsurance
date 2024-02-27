09c8c1d042da6c3be5f51baccabfdac6154dbd51
Gerald Ferreira <zaluckyb@gmail.com>
2024-02-27 12:59:42 +0200

    warehouse

---
+++ Hollard/Assets/WarehouseConstructionSet/Source/Scripts/Traffic.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traffic : MonoBehaviour {
+
+	public Texture2D open;
+	public Texture2D closed;
+
+	public GameObject trafficLight;
+	public GameObject orangeLight;
+
+	public Animator garageDoor;
+
+	public bool isOpen = false;
+
+	Light pointLight;
+	Light spotLight;
+
+	Renderer interior_unit;
+	Renderer exterior_unit;
+	Renderer r_orangeLamp;
+
+	void Start(){
+		pointLight = trafficLight.GetComponentInChildren<Light> ();
+		interior_unit = this.GetComponent<Renderer> ();
+		exterior_unit = trafficLight.GetComponent<Renderer> ();
+		r_orangeLamp = orangeLight.GetComponent<Renderer> ();
+		spotLight = orangeLight.GetComponentInChildren<Light> ();
+	}
+
+	public void ToggleGarageGates() {
+		isOpen = !isOpen;
+
+		if (isOpen) {
+
+			StartCoroutine (OrangeLight ());
+			interior_unit.material.SetTexture ("_EmissionMap", open);
+			exterior_unit.material.SetTexture ("_EmissionMap", open);
+			Color c = Color.red * Mathf.LinearToGammaSpace (7f);
+			DynamicGI.SetEmissive (interior_unit, c);
+			DynamicGI.SetEmissive (exterior_unit, c);
+			pointLight.color = Color.red;
+
+		} else {
+			StartCoroutine (OrangeLight ());
+			exterior_unit.material.SetTexture ("_EmissionMap", closed);
+			interior_unit.material.SetTexture ("_EmissionMap", closed);
+			Color c = Color.red * Mathf.LinearToGammaSpace (7f);
+			DynamicGI.SetEmissive (interior_unit, c);
+			DynamicGI.SetEmissive (exterior_unit, c);
+			pointLight.color = Color.green;
+
+		}
+
+
+		garageDoor.SetBool ("open", isOpen);
+
+
+	
+	}
+
+	IEnumerator OrangeLight() {
+		spotLight.enabled = true;
+		Color c = Color.yellow * Mathf.LinearToGammaSpace(10f);
+		r_orangeLamp.material.SetColor ("_EmissionColor", c);
+		DynamicGI.SetEmissive (r_orangeLamp, c);
+		yield return new WaitForSeconds (3f);
+		c = c * Mathf.LinearToGammaSpace (0.0001f);
+		r_orangeLamp.material.SetColor ("_EmissionColor", c);
+		DynamicGI.SetEmissive (r_orangeLamp, c);
+		spotLight.enabled = false;
+	}
+}
---
+++ Hollard/Assets/WarehouseConstructionSet/Source/Scripts/WH_Doors.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WH_Doors : MonoBehaviour {
+
+	Camera cam;
+	Animator anim;
+
+	public bool showCrosshair = true;
+	public Texture2D crosshair_Default;
+	public Texture2D crosshair_Yellow;
+
+	bool inRange = false;
+
+	[Range(0.5f, 10f)]
+	public float interactionDistance = 3f;
+
+
+	void Start(){
+		cam = FindObjectOfType<Camera> ();
+	}
+
+	void Update(){
+
+		Ray ray = cam.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0));
+		RaycastHit hit;
+
+		if (Physics.Raycast (ray, out hit, interactionDistance)) {
+			if (hit.transform.GetComponent<Door_ID> ()) {
+				//TODO Door animation
+				Door_ID id = hit.transform.GetComponent<Door_ID> ();
+				inRange = true;
+				anim = id.GetComponent<Animator> ();
+
+				if (Input.GetKeyDown (KeyCode.E)) {
+					
+					anim.SetBool ("open", !anim.GetBool ("open"));
+
+				}
+			} else if (hit.transform.GetComponent<Traffic>()) {
+				//TODO Large gates opening
+				inRange = true;
+
+				if (Input.GetKeyDown (KeyCode.E)) {
+					hit.transform.GetComponent<Traffic> ().ToggleGarageGates ();
+				}
+				//Other components go below
+			}else {
+				inRange = false;
+			}
+		} else {
+			inRange = false;
+		}
+	}
+
+	void OnGUI(){
+		
+		Rect rect = new Rect (Screen.width / 2 - crosshair_Default.width /2 , Screen.height / 2 - crosshair_Default.height /2, crosshair_Default.width, crosshair_Default.height);
+
+		if (showCrosshair) {
+			if (inRange) {
+				GUI.DrawTexture (rect, crosshair_Yellow);
+			} else {
+				GUI.DrawTexture (rect, crosshair_Default);
+			}
+		}
+
+	}
+}
